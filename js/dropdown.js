f9a6a65d773e13dedfe6be69cf01435a272c90d2
Marina <marina.enadeghe@gmail.com>
2018-07-13 00:42:24 +0200

    tagesprogramm grid

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,3 +1,12 @@
 function dropdownTip(value){
     document.getElementById("result").innerHTML = value;
 }
+
+/*
+gelöscht: select und das div mit result
+
+Idee: onclick zeige Inhalt von etwas mit id sowieso und setze class="xy"
+class btnweiss bzw. btnschwarz ist für styling zuständig
+
+Bsp.: click id's: freitag -> div id ist show_freitag
+ */
