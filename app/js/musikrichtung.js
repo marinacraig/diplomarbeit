c5d4c60aa4d9eafaaa6b4f1068a02456551ef6ef
Marina <marina.enadeghe@gmail.com>
2018-08-15 23:36:51 +0200

    inkl. bunten filter

--- app/js/musikrichtung.js
+++ app/js/musikrichtung.js
@@ -1,5 +1,23 @@
 let musikfilter = document.getElementById('musikfilter');
 if (musikfilter!==null) {
+/*
+Idee:
+merke von welcher Seite jemand kommt
+ist filter angeglickt?
+welcher?
+geändert?
+neu?
+speichern im Profil falls eingeloggt
+speichern als Cookie und entsprechend abrufen
 
+Seiten:
+    1. index.html
+    2. festivaluebersicht.html
+    (3. dashboard)
+    [4. musikrichtung.html]
+
+
+    falls jemand auf filter zurücksetzen klickt -> alle "löschen"
+ */
 
 }
