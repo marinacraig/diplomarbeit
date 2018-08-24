32b6becf2db4b4a1c376f859f9bcf6b4d994bf6f
Marina <marina.enadeghe@gmail.com>
2018-08-24 13:55:00 +0200

    bugfix ticketbestellen - css

---
+++ app/js/counter.js
@@ -0,0 +1,19 @@
+/*
+Da Codeschnipsel für ticketbestellen:
+check ob id xy vorhanden, falls ja dann ausführen
+ */
+
+let ticketsBestellen = document.getElementById('ticketsBestellen');
+if (ticketsBestellen!==null) {
+
+/*
+Idee:
+Einzele Ticket-Variante
+Bei klick auf plus: plus 1
+bei klick auf minus: minus 1
+-> Total dazwischen anzeigen
+
+später: überprüfen ob tickets vorhanden, falls nicht -> anderen Text (grau) einfügen
+ */
+
+}
---
+++ app/php/register.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * Idee: prüfen ob schon eingeloggt
+ * wenn nein
+ * prüfen ob login schon vorhanden
+ * -> falls ja: Fehlermeldung? oder zurück?
+ * -> unbedingt password hashen
+ * falls registrieren ok:
+ * 1. überprüfen ob im localStorage Daten von FMS (=Favorite Music Style) vorhanden
+ *      falls ja, ebenfalls speichern
+ * 2. geklappt: laut styleguide ins dashboard,
+ * da dies nicht vorhanden -> ticketuebersicht.html (kann dann "merken")
+ */
+
+
+//Session
+
+
+//Passwort hashen etc
