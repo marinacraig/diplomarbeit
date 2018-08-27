a61e82b75774820b1679bb7e417497a9fd5b431d
Marina <marina.enadeghe@gmail.com>
2018-08-27 18:54:28 +0200

    alles da ausser slider

--- app/js/slider.js
+++ app/js/slider.js
@@ -1,10 +1,10 @@
-/*
+gulp/*
 Da Codeschnipsel für slider auf der Festivalseite:
 check ob id xy vorhanden, falls ja dann ausführen
  */
 
 let slider = document.getElementById('slider');
-if (slider!==null) {
+if (slider !==null) {
 
 
     var $slider = $('.slider');
